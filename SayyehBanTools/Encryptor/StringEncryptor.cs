47fd06abe0aefc0f94b3b8f6f8f7ce7c98bc5d02
SayyehBan <sdvp1991david@gmail.com>
2024-02-28 17:11:14 +0330

    رمزگذاری و رمزگشایی به صورت استاتیک

--- SayyehBanTools/Encryptor/StringEncryptor.cs
+++ SayyehBanTools/Encryptor/StringEncryptor.cs
@@ -3,28 +3,22 @@ using System.Text;
 
 namespace SayyehBanTools.Encryptor;
 
-public class Encryptor
+public static class StringEncryptor
 {
-    private readonly string initVector;
-    private readonly int keysize;
-    private readonly string passPhrase;
-    private string _plainText;
-    public Encryptor()
-    {
-        initVector = "tu89geji340t89u2";
-        passPhrase = "SayyehBanString";
-        keysize = 256;
-    }
-    private string Encrypt(string plainText)
+    private static readonly string initVector = "tu89geji340t89u2";
+    private static readonly string passPhrase = "SayyehBanstring";
+    private static readonly int keysize = 256;
+
+    public static string Encrypt(string plainText)
     {
         byte[] bytes1 = Encoding.UTF8.GetBytes(initVector);
         byte[] bytes2 = Encoding.UTF8.GetBytes(plainText);
         byte[] bytes3 = new PasswordDeriveBytes(passPhrase, null).GetBytes(keysize / 8);
-        Rijndael managed = new RijndaelManaged
+        RijndaelManaged rijndaelManaged = new RijndaelManaged
         {
             Mode = CipherMode.CBC
         };
-        ICryptoTransform encryptor = managed.CreateEncryptor(bytes3, bytes1);
+        ICryptoTransform encryptor = rijndaelManaged.CreateEncryptor(bytes3, bytes1);
         MemoryStream memoryStream = new MemoryStream();
         CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
         cryptoStream.Write(bytes2, 0, bytes2.Length);
@@ -35,16 +29,16 @@ public class Encryptor
         return Convert.ToBase64String(array);
     }
 
-    private string Decrypt(string cipherText)
+    public static string Decrypt(string cipherText)
     {
         byte[] bytes1 = Encoding.ASCII.GetBytes(initVector);
         byte[] buffer = Convert.FromBase64String(cipherText);
         byte[] bytes2 = new PasswordDeriveBytes(passPhrase, null).GetBytes(keysize / 8);
-        RijndaelManaged managed = new RijndaelManaged
+        RijndaelManaged rijndaelManaged = new RijndaelManaged
         {
             Mode = CipherMode.CBC
         };
-        ICryptoTransform decryptor = managed.CreateDecryptor(bytes2, bytes1);
+        ICryptoTransform decryptor = rijndaelManaged.CreateDecryptor(bytes2, bytes1);
         MemoryStream memoryStream = new MemoryStream(buffer);
         CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
         byte[] numArray = new byte[buffer.Length];
@@ -53,32 +47,31 @@ public class Encryptor
         cryptoStream.Close();
         return Encoding.UTF8.GetString(numArray, 0, count);
     }
-    public string PlainText
+
+    public static string DecryptConnectionString(string plainText)
     {
-        set => _plainText = value;
-    }
-    public string EncryptText => Encrypt(_plainText);
-    public string DecryptConnectionString
-    {
-        get
+        try
         {
-            int num1 = _plainText.IndexOf("||");
+            int num1 = plainText.IndexOf("||");
             if (num1 == -1)
             {
-                return Decrypt(_plainText);
+                return Decrypt(plainText);
             }
+
             int num2 = num1 + 1;
-            string str = _plainText.Substring(0, num2 - 1);
-            string cipherText = _plainText.Substring(num2 + 1, _plainText.Length - (num2 + 1));
-            try
-            {
-                return str + Decrypt(cipherText);
-            }
-            catch (Exception)
-            {
-                return string.Empty;
-            }
+            string str = plainText.Substring(0, num2 - 1);
+            string cipherText = plainText.Substring(num2 + 1, plainText.Length - (num2 + 1));
+            return str + Decrypt(cipherText);
+        }
+        catch (Exception)
+        {
+            return string.Empty;
         }
     }
-    public string DecryptText => Decrypt(_plainText);
 }
+//string plainText = "متن مورد نظر برای رمزنگاری";
+//string encryptedText = StringEncryptor.Encrypt(plainText);
+//string encryptedText = "متن رمزنگاری شده";
+//string plainText = StringEncryptor.Decrypt(encryptedText);
+//string plainText = "ConnectionString||密文";
+//string connectionString = StringEncryptor.DecryptConnectionString(plainText);
