3d5b26813b5ade6873684164277d81959996ab9e
SayyehBan <sdvp1991david@gmail.com>
2024-03-31 23:18:52 +0330

    افزودن ارتباط با RabbitMQ

---
+++ SayyehBanTools/ConnectionDB/RabbitMQConnection.cs
@@ -0,0 +1,21 @@
+﻿using SayyehBanTools.Encryptor;
+
+namespace SayyehBanTools.ConnectionDB;
+
+public class RabbitMQConnection
+{
+    public static string DefaultConnection()
+    {
+        string connection = "amqp://guest:guest@localhost:5672";
+        return connection;
+    }
+    public static string DynamicConnection(string Username, string Password, string Url, string Port, string InitVector, string PassPhrase)
+    {
+        string username = StringEncryptor.Decrypt(Username, InitVector, PassPhrase);
+        string password = StringEncryptor.Decrypt(Password, InitVector, PassPhrase);
+        string url = StringEncryptor.Decrypt(Url, InitVector, PassPhrase);
+        string port = StringEncryptor.Decrypt(Port, InitVector, PassPhrase);
+        string connection = $"amqp://{username}:{password}@{url}:{port}";
+        return connection;
+    }
+}
