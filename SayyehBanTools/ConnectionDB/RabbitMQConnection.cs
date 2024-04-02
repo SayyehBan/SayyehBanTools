747a6a2db8f82ea23308817ba34202f4e2f9377b
SayyehBan <sdvp1991david@gmail.com>
2024-04-02 14:27:44 +0330

    پیاده سازی CloudAMQP

--- SayyehBanTools/ConnectionDB/RabbitMQConnection.cs
+++ SayyehBanTools/ConnectionDB/RabbitMQConnection.cs
@@ -11,13 +11,23 @@ public class RabbitMQConnection
         return uri;
     }
 
-    public static Uri DynamicConnection(string Username, string Password, string Url, string Port, string InitVector, string PassPhrase)
+    public static Uri RabbitMQ(string Username, string Password, string Url, string Port, string InitVector, string PassPhrase)
     {
         string username = StringEncryptor.Decrypt(Username, InitVector, PassPhrase);
         string password = StringEncryptor.Decrypt(Password, InitVector, PassPhrase);
         string url = StringEncryptor.Decrypt(Url, InitVector, PassPhrase);
         string port = StringEncryptor.Decrypt(Port, InitVector, PassPhrase);
         string connection = $"amqp://{username}:{password}@{url}:{port}";
+        Uri uri = new Uri(connection);
+        return uri;
+    }
+    public static Uri CloudAMQP(string Username, string Password, string Url,  string InitVector, string PassPhrase)
+    {
+        string username = StringEncryptor.Decrypt(Username, InitVector, PassPhrase);
+        string password = StringEncryptor.Decrypt(Password, InitVector, PassPhrase);
+        string url = StringEncryptor.Decrypt(Url, InitVector, PassPhrase);
+     
+        string connection = $"amqps://{username}:{password}@{url}";
         Uri uri = new Uri(connection);
         return uri;
     }
