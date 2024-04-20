15949467b995006b6da7a0290985b91390a65474
SayyehBan <sdvp1991david@gmail.com>
2024-04-20 14:45:32 +0330

    جدا سازی سرویس RabbitMQ

--- SayyehBanTools/ConfigureService/ConfigureServicesRabbitMQ.cs
+++ SayyehBanTools/ConfigureService/ConfigureServicesRabbitMQ.cs
@@ -4,18 +4,16 @@ using SayyehBanTools.MessagingBus.RabbitMQ.SendMessage;
 
 namespace SayyehBanTools.ConfigureService;
 
-public class ConfigureServices
+public class ConfigureServicesRabbitMQ
 {
     public void ConfigureService(IServiceCollection services)
     {
         services.AddTransient<RabbitMQConnection, RabbitMQConnection>();
-        services.AddTransient<RabbitMQConnectionNormal, RabbitMQConnectionNormal>();
         services.AddTransient<ISendMessages, RabbitMQMessageBus>();
-        services.AddTransient<ISendMessages, RabbitMQMessageBusNormal>();
     }
 }
 /*
 * طریقه صدا زدن سرویس ها
-var configureServices = new ConfigureServices();
+var configureServices = new ConfigureServicesRabbitMQ();
 configureServices.ConfigureService(builder.Services);
 */
---
+++ SayyehBanTools/ConfigureService/ConfigureServicesRabbitMQNormal.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SayyehBanTools.MessagingBus.RabbitMQ.Connection;
+using SayyehBanTools.MessagingBus.RabbitMQ.SendMessage;
+
+namespace SayyehBanTools.ConfigureService;
+
+public class ConfigureServicesRabbitMQNormal
+{
+    public void ConfigureService(IServiceCollection services)
+    {
+        services.AddTransient<RabbitMQConnectionNormal, RabbitMQConnectionNormal>();
+        services.AddTransient<ISendMessages, RabbitMQMessageBusNormal>();
+    }
+}
