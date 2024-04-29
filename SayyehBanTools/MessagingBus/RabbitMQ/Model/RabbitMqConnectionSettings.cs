9523f0c0b8a45cba2dc2831ca6256037d538d5c4
SayyehBan <sdvp1991david@gmail.com>
2024-04-29 18:19:31 +0330

    برداشت صف از لیستبه علت زیاد وبودن و متنوع بودن صف

--- SayyehBanTools/MessagingBus/RabbitMQ/Model/RabbitMqConnectionSettings.cs
+++ SayyehBanTools/MessagingBus/RabbitMQ/Model/RabbitMqConnectionSettings.cs
@@ -6,7 +6,6 @@ public class RabbitMqConnectionSettings
     public string Port { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public string queue { get; set; }
     public string InitVector { get; set; }
     public string PassPhrase { get; set; }
 }
--- SayyehBanTools/MessagingBus/RabbitMQ/Model/RabbitMqConnectionSettingsNormal.cs
+++ SayyehBanTools/MessagingBus/RabbitMQ/Model/RabbitMqConnectionSettingsNormal.cs
@@ -6,7 +6,6 @@ public class RabbitMqConnectionSettingsNormal
     public string Port { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public string queue { get; set; }
 }
 
 /*
