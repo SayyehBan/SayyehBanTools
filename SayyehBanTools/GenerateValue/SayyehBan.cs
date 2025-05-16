12a0c2140e8f51386b0b0751cf3be0b4170c1043
سایه بان <38620223+SayyehBan@users.noreply.github.com>
2025-05-16 07:34:31 +0330

    تست SayyehBan

--- SayyehBanTools/GenerateValue/SayyehBan.cs
+++ SayyehBanTools/GenerateValue/SayyehBan.cs
@@ -1,130 +1,143 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 /// <summary>
-/// کلاس بررسی و تولید کد سایه بان
+/// کلاس بررسی و تولید کد سایه‌بان
 /// </summary>
 public class SayyehBan
 {
     /// <summary>
-    /// بررسی کد سایه بان
+    /// بررسی صحت کد سایه‌بان
     /// </summary>
-    /// <param name="nationalCode"></param>
-    /// <returns></returns>
-    /// <exception cref="Exception"></exception>
-    public static Boolean IsValidCode(String nationalCode)
+    /// <param name="nationalCode">کد سایه‌بان 13 رقمی</param>
+    /// <returns>درستی یا نادرستی کد</returns>
+    /// <exception cref="Exception">در صورت نادرست بودن فرمت کد</exception>
+    public static bool IsValidCode(string nationalCode)
     {
-        //در صورتی که کد ملی وارد شده تهی باشد
+        // بررسی تهی بودن
+        if (string.IsNullOrEmpty(nationalCode))
+            throw new Exception("لطفا کد سایه‌بان را صحیح وارد نمایید");
 
-        if (String.IsNullOrEmpty(nationalCode))
-            throw new Exception("لطفا کد سایه بان را صحیح وارد نمایید");
-
-
-        //در صورتی که کد ملی وارد شده طولش کمتر از 10 رقم باشد
+        // بررسی طول کد
         if (nationalCode.Length != 13)
-            throw new Exception("طول کد سایه بان باید سیزده کاراکتر باشد");
+            throw new Exception("طول کد سایه‌بان باید سیزده کاراکتر باشد");
 
-        //در صورتی که کد ملی ده رقم عددی نباشد
+        // بررسی عددی بودن
         var regex = new Regex(@"\d{13}");
         if (!regex.IsMatch(nationalCode))
-            throw new Exception("کد سایه بان تشکیل شده از سیزده رقم عددی می‌باشد؛ لطفا کد سایه بان را صحیح وارد نمایید");
+            throw new Exception("کد سایه‌بان باید شامل سیزده رقم عددی باشد");
 
-        //در صورتی که رقم‌های کد ملی وارد شده یکسان باشد
-        var allDigitEqual = new[] { "0000000000000",
-                                        "1111111111111",
-                                        "2222222222222",
-                                        "3333333333333",
-                                        "4444444444444",
-                                        "5555555555555",
-                                        "6666666666666",
-                                        "7777777777777",
-                                        "8888888888888",
-                                        "9999999999999" };
-        if (allDigitEqual.Contains(nationalCode)) return false;
+        // بررسی یکسان بودن تمام ارقام
+        var allDigitEqual = new[] { "0000000000000", "1111111111111", "2222222222222", "3333333333333",
+                                    "4444444444444", "5555555555555", "6666666666666", "7777777777777",
+                                    "8888888888888", "9999999999999" };
+        if (allDigitEqual.Contains(nationalCode))
+            return false;
 
-
-        //عملیات شرح داده شده در بالا
+        // محاسبه الگوریتم
         var chArray = nationalCode.ToCharArray();
-        var num0 = Convert.ToInt32(chArray[0].ToString()) * 13;
-        var num2 = Convert.ToInt32(chArray[1].ToString()) * 12;
-        var num3 = Convert.ToInt32(chArray[2].ToString()) * 11;
-        var num4 = Convert.ToInt32(chArray[3].ToString()) * 10;
-        var num5 = Convert.ToInt32(chArray[4].ToString()) * 9;
-        var num6 = Convert.ToInt32(chArray[5].ToString()) * 8;
-        var num7 = Convert.ToInt32(chArray[6].ToString()) * 7;
-        var num8 = Convert.ToInt32(chArray[7].ToString()) * 6;
-        var num9 = Convert.ToInt32(chArray[8].ToString()) * 5;
-        var num10 = Convert.ToInt32(chArray[9].ToString()) * 4;
-        var num11 = Convert.ToInt32(chArray[10].ToString()) * 3;
-        var num12 = Convert.ToInt32(chArray[11].ToString()) * 2;
-        var a = Convert.ToInt32(chArray[12].ToString());
-
-        var b = ((((((((((num0 + num2) + num3) + num4) + num5) + num6) + num7) + num8) + num9) + num10) + num11) + num12;
-        var c = b % 14;
-
-        return (((c < 2) && (a == c)) || ((c >= 2) && ((14 - c) == a)));
-    }
-    ///نحوه بررسی کد سایه بان
-    ///   if (!IsValidCode(textBox2.Text))
-    //{
-    //    MessageBox.Show("کد شما صحیح نیست");
-    //}
-    //if (IsValidCode(textBox2.Text))
-    //{
-    //    MessageBox.Show("کد شما صحیح است");
-    //}
-    public static string GenerateCode()
-    {
-        string ss = "";
-        string number123 = "01234567890123";
-        Random rnd = new Random();
-        object code1 = rnd.Next(1, 12);
-        object code2 = rnd.Next(1, 12);
-        object code3 = rnd.Next(1, 12);
-        object code4 = rnd.Next(1, 12);
-        object code5 = rnd.Next(1, 12);
-        object code6 = rnd.Next(1, 12);
-        object code7 = rnd.Next(1, 12);
-        object code8 = rnd.Next(1, 12);
-        object code9 = rnd.Next(1, 12);
-        object code10 = rnd.Next(1, 12);
-        object code11 = rnd.Next(1, 12);
-        object code12 = rnd.Next(1, 12);
-        string numbers1 = number123.Substring(Convert.ToInt32(code1), 1);
-        string numbers2 = number123.Substring(Convert.ToInt32(code2), 1);
-        string numbers3 = number123.Substring(Convert.ToInt32(code3), 1);
-        string numbers4 = number123.Substring(Convert.ToInt32(code4), 1);
-        string numbers5 = number123.Substring(Convert.ToInt32(code5), 1);
-        string numbers6 = number123.Substring(Convert.ToInt32(code6), 1);
-        string numbers7 = number123.Substring(Convert.ToInt32(code7), 1);
-        string numbers8 = number123.Substring(Convert.ToInt32(code8), 1);
-        string numbers9 = number123.Substring(Convert.ToInt32(code9), 1);
-        string numbers10 = number123.Substring(Convert.ToInt32(code10), 1);
-        string numbers11 = number123.Substring(Convert.ToInt32(code11), 1);
-        string numbers12 = number123.Substring(Convert.ToInt32(code12), 1);
-        string sumnumber = numbers1 + numbers2 + numbers3 + numbers4 + numbers5 + numbers6 + numbers7 + numbers8 + numbers9 + numbers10 + numbers11 + numbers12;
-        var code = sumnumber.ToArray();
-        if (code.Length == 12)
+        int sum = 0;
+        int[] weights = { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        for (int i = 0; i < 12; i++)
         {
-            int numberPosition = 13;
-            int sum = 0;
-            while (numberPosition >= 2)
-            {
-                for (int i = 0; i <= 11; i++)
-                {
-                    int number = Convert.ToInt32(code[i].ToString()) * numberPosition;
-                    sum = sum + number;
-                    numberPosition--;
-                }
-            }
-            int numberControl = (14 - (sum % 14));
-            ss = sumnumber + Convert.ToString(numberControl);
-            if (ss.Length == 13)
-            {
-            }
+            sum += Convert.ToInt32(chArray[i].ToString()) * weights[i];
+        }
+        int a = Convert.ToInt32(chArray[12].ToString());
+        int c = sum % 14;
+
+        return (c < 2 && a == c) || (c >= 2 && (14 - c) == a);
+    }
+
+    /// <summary>
+    /// تولید کد سایه‌بان تصادفی و معتبر
+    /// </summary>
+    /// <returns>یک کد سایه‌بان 13 رقمی معتبر</returns>
+    public static string GenerateRandom()
+    {
+        Random rnd = new Random();
+        int[] digits = new int[12];
+
+        // تولید 12 رقم اول به‌صورت تصادفی
+        for (int i = 0; i < 12; i++)
+        {
+            digits[i] = rnd.Next(0, 10); // اعداد بین 0 تا 9
         }
 
-        return ss;
+        // محاسبه مجموع وزن‌دار
+        int sum = 0;
+        int[] weights = { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        for (int i = 0; i < 12; i++)
+        {
+            sum += digits[i] * weights[i];
+        }
+
+        // محاسبه رقم کنترلی
+        int c = sum % 14;
+        int controlDigit = c < 2 ? c : 14 - c;
+
+        // اگر controlDigit دو رقمی است، دوباره تولید کن
+        if (controlDigit > 9)
+        {
+            return GenerateRandom();
+        }
+
+        // ترکیب ارقام
+        string code = string.Join("", digits) + controlDigit;
+
+        // اطمینان از معتبر بودن کد
+        if (!IsValidCode(code))
+        {
+            return GenerateRandom();
+        }
+
+        return code;
     }
-    //نحوه استفاده از کد تولید
-    // textBox1.Text = GenerateCode();
+
+    /// <summary>
+    /// تولید کد سایه‌بان رند و معتبر
+    /// </summary>
+    /// <returns>یک کد سایه‌بان 13 رقمی معتبر با الگوی رند</returns>
+    public static string GenerateRound()
+    {
+        Random rnd = new Random();
+        int[] digits = new int[12];
+
+        // الگوی رند: شروع با "00" و ادامه با اعداد محدود
+        digits[0] = 0; // رقم اول
+        digits[1] = 0; // رقم دوم
+        for (int i = 2; i < 12; i++)
+        {
+            digits[i] = rnd.Next(0, 5); // محدود به 0 تا 4 برای الگوی منظم‌تر
+        }
+
+        // محاسبه مجموع وزن‌دار
+        int sum = 0;
+        int[] weights = { 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        for (int i = 0; i < 12; i++)
+        {
+            sum += digits[i] * weights[i];
+        }
+
+        // محاسبه رقم کنترلی
+        int c = sum % 14;
+        int controlDigit = c < 2 ? c : 14 - c;
+
+        // اگر controlDigit دو رقمی است، دوباره تولید کن
+        if (controlDigit > 9)
+        {
+            return GenerateRound();
+        }
+
+        // ترکیب ارقام
+        string code = string.Join("", digits) + controlDigit;
+
+        // اطمینان از معتبر بودن کد
+        if (!IsValidCode(code))
+        {
+            return GenerateRound();
+        }
+
+        return code;
+    }
 }
---
+++ SayyehBanToolsTest/GenerateValue/SayyehBanTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xunit;
+
+public class SayyehBanTests
+{
+    [Fact]
+    public void IsValidCode_ValidCode_ReturnsTrue()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string validCode = "0010403122014"; // یک کد معتبر فرضی
+        bool result = SayyehBan.IsValidCode(validCode);
+        Assert.True(result, "کد معتبر باید true برگرداند.");
+    }
+
+    [Fact]
+    public void IsValidCode_AllDigitsSame_ReturnsFalse()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string invalidCode = "1111111111111";
+        bool result = SayyehBan.IsValidCode(invalidCode);
+        Assert.False(result, "کدی که تمام ارقام آن یکسان است باید false برگرداند.");
+    }
+
+    [Fact]
+    public void IsValidCode_NullOrEmpty_ThrowsException()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        var exception = Assert.Throws<Exception>(() => SayyehBan.IsValidCode(""));
+        Assert.Equal("لطفا کد سایه‌بان را صحیح وارد نمایید", exception.Message);
+    }
+
+    [Fact]
+    public void IsValidCode_WrongLength_ThrowsException()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        var exception = Assert.Throws<Exception>(() => SayyehBan.IsValidCode("1234567890"));
+        Assert.Equal("طول کد سایه‌بان باید سیزده کاراکتر باشد", exception.Message);
+    }
+
+    [Fact]
+    public void IsValidCode_NonNumeric_ThrowsException()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        var exception = Assert.Throws<Exception>(() => SayyehBan.IsValidCode("1234567890abc"));
+        Assert.Equal("کد سایه‌بان باید شامل سیزده رقم عددی باشد", exception.Message);
+    }
+
+    [Fact]
+    public void GenerateRandom_Returns13DigitCode()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string code = SayyehBan.GenerateRandom();
+        Assert.Equal(13, code.Length);
+        Assert.Matches(@"^\d{13}$", code);
+    }
+
+    [Fact]
+    public void GenerateRandom_ProducesValidCode()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string code = SayyehBan.GenerateRandom();
+        bool isValid = SayyehBan.IsValidCode(code);
+        Assert.True(isValid, "کد تولیدشده توسط GenerateRandom باید معتبر باشد.");
+    }
+
+    [Fact]
+    public void GenerateRound_Returns13DigitCode()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string code = SayyehBan.GenerateRound();
+        Assert.Equal(13, code.Length);
+        Assert.Matches(@"^\d{13}$", code);
+    }
+
+    [Fact]
+    public void GenerateRound_ProducesValidCode()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string code = SayyehBan.GenerateRound();
+        bool isValid = SayyehBan.IsValidCode(code);
+        Assert.True(isValid, "کد تولیدشده توسط GenerateRound باید معتبر باشد.");
+    }
+
+    [Fact]
+    public void GenerateRound_StartsWith00AndUsesLimitedDigits()
+    {
+        // ترتیب: آماده‌سازی، اجرا، تأیید
+        string code = SayyehBan.GenerateRound();
+        Assert.StartsWith("00", code);
+        for (int i = 2; i < 12; i++)
+        {
+            int digit = int.Parse(code[i].ToString());
+            Assert.InRange(digit, 0, 4);
+        }
+    }
+}
